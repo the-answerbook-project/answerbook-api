fb22ae1e70f2dacc62043a6d8e6c10c5a5feb7f4
jsbailey <jason.bailey@imperial.ac.uk>
2024-06-06 16:54:56 +0100

    Feat: Add AnswerRead response model for answer endpoint

--- api/router/exam.py
+++ api/router/exam.py
@@ -3,7 +3,7 @@ from sqlmodel import Session, select
 from starlette.status import HTTP_404_NOT_FOUND
 
 from api.dependencies import get_assessment, get_session
-from api.schemas.answer import Answer
+from api.schemas.answer import Answer, AnswerRead
 from api.schemas.exam import Assessment, AssessmentSummary, Question
 
 exam_router = APIRouter()
@@ -21,31 +21,34 @@ The response include exam metadata, such as start time and end time for the curr
 """,
 )
 def get_question(
-    question_number: int,
-    assessment: Assessment = Depends(get_assessment),
+        question_number: int,
+        assessment: Assessment = Depends(get_assessment),
 ):
     if question_number not in assessment.questions:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Question not found")
     return assessment.questions[question_number]
 
-# ------------------------------------------ Answers
 
+# ------------------------------------------ Answers
 
 
 @exam_router.get(
     "/questions/{question_number}/answer",
-    tags=["exam"],
+    tags=["answer"],
+    response_model=list[AnswerRead],
     summary="Retrieve answer for question by user",
     description="""
 Retrieve the given latest answer by a user including part, section, task.
 """,
 )
 def get_answer(
-    question_number: int,
-session: Session = Depends(get_session),
+        question_number: int,
+        session: Session = Depends(get_session),
 
 ):
-    query = select(Answer)
+    query = select(Answer).where(Answer.question == question_number, Answer.username == "hpotter").order_by(Answer.part,
+                                                                                                            Answer.section,
+                                                                                                            Answer.task)
     answers = session.exec(query).all()
 
     return answers
@@ -61,6 +64,6 @@ Retrieve the exam summary with user-specific start-time and end-time.
 """,
 )
 def get_summary(
-    assessment: Assessment = Depends(get_assessment),
+        assessment: Assessment = Depends(get_assessment),
 ):
     return AssessmentSummary(**assessment.dict())
