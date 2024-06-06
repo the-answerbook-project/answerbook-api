af664ab40952492dcd4cdb0cc05d48871f87c79e
jsbailey <jason.bailey@imperial.ac.uk>
2024-06-06 14:15:30 +0100

    Fix: Remove commented out txt

--- tests/api/test_exam.py
+++ tests/api/test_exam.py
@@ -30,27 +30,7 @@ def test_can_get_question_for_exam(client):
 def test_can_get_answer_for_question_by_user(web_client):
     res = web_client.get("/questions/1/answer")
     assert res.status_code == 200
-    # q = res.json()
-    # assert q["title"] == "Title of the question"
-    # assert q["instructions"] == "Some instructions for this question."
-    # assert len(q["parts"]) == 1
-    # assert q["parts"]["1"]["instructions"] == "Some instructions for this part."
-    # sections = q["parts"]["1"]["sections"]
-    # assert len(sections) == 1
-    # assert sections["1"]["instructions"] == "Some instructions for this section."
-    # assert sections["1"]["maximum_mark"] == 10
-    # tasks = sections["1"]["tasks"]
-    # assert len(tasks) == 2
-    # [task1, task2] = tasks
-    # assert task1["type"] == TaskType.ESSAY
-    # assert task1["lines"] == 5
-    # assert task2["instructions"] == "Some instructions for this task."
-    # assert task2["type"] == TaskType.MULTIPLE_CHOICE_SELECT_ONE
-    # assert task2["choices"] == [
-    #     {"value": "a", "label": "Red pill"},
-    #     {"value": "b", "label": "Blue pill"},
-    # ]
-
+    assert res.json() == "answer"
 
 
 
