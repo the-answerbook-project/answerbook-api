25f69efcc9aaa05585e1a667a2ebff59dbe7718f
jsbailey <jason.bailey@imperial.ac.uk>
2024-06-06 14:27:25 +0100

    Feat: Add answer model

---
+++ api/schemas/answer.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+import sqlmodel
+from sqlmodel import SQLModel, Field
+
+
+class Answer(SQLModel):
+    id: int | None = Field(primary_key=True, default=None)
+
+    exam_id: int = Field(default=None)  # TODO:: FK?
+
+    username: str = Field(nullable=False)
+    question: int = Field(default=None)
+    part: int = Field(default=None)
+    section: int = Field(default=None)
+    task: int = Field(default=None)
+
+    answer: str = Field(nullable=True)
+
+    timestamp: datetime = Field(
+        sa_column=sqlmodel.Column(
+            sqlmodel.DateTime(timezone=False),  # TODO:: True or false?
+        )
+    )
+
+    ip: str = Field(nullable=True)  # TODO:: inet
