063bea3cb5e563636259cf939727d153e334401c
jsbailey <jason.bailey@imperial.ac.uk>
2024-06-12 14:04:39 +0100

    feat: add and skip test, testing for marks history

--- tests/api/test_mark.py
+++ tests/api/test_mark.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_can_get_user_marks_for_question(client, mark_feedback_factory):
     mark_feedback_factory.create_batch(
         size=3, exam_id="y2023_12345_exam", question=1, username="hpotter"
@@ -27,3 +30,18 @@ def test_gets_empty_list_response_if_no_marks_exist_for_assessment(client):
     res = client("y2023_12345_exam").get("/questions/1/mark")
     assert res.status_code == 200
     assert len(res.json()) == 0
+
+
+@pytest.mark.skip(reason="mark_id does not get generated when calling for history")
+def test_can_get_user_marks_and_marks_history_for_question(
+    client, mark_feedback_factory
+):
+    mark_feedback_factory(
+        exam_id="y2023_12345_exam", question=1, username="hpotter", with_history=5
+    )
+
+    res = client("y2023_12345_exam").get("/questions/1/mark")
+    assert res.status_code == 200
+
+    [mark_] = res.json()
+    assert len(mark_["history"]) == 5
