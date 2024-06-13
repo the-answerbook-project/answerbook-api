b643d2467f94349abb2f078113168bbb78933f40
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-06-13 16:49:39 +0100

    feat: add endpoints returning all questions for an assessment

--- api/router/exam.py
+++ api/router/exam.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from api.dependencies import get_assessment
-from api.schemas.exam import Assessment, AssessmentSummary
+from api.schemas.exam import Assessment, AssessmentSummary, Question
 
 exam_router = APIRouter(tags=["exam"])
 
@@ -10,7 +10,7 @@ exam_router = APIRouter(tags=["exam"])
     "/summary",
     tags=["exam"],
     response_model=AssessmentSummary,
-    summary="Retrieve exam summary information",
+    summary="Exam summary information",
     description="""
 Retrieve the exam summary with user-specific start-time and end-time.
 """,
@@ -19,3 +19,16 @@ def get_summary(
     assessment: Assessment = Depends(get_assessment),
 ):
     return AssessmentSummary(**assessment.dict())
+
+
+@exam_router.get(
+    "/questions",
+    tags=["exam"],
+    response_model=dict[int, Question],
+    summary="Exam questions",
+    description="Retrieve all the exam questions.",
+)
+def get_questions(
+    assessment: Assessment = Depends(get_assessment),
+):
+    return assessment.questions
--- tests/api/test_exam.py
+++ tests/api/test_exam.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_can_get_summary_for_exam(client):
     res = client("simple").get("/summary")
     assert res.status_code == 200
@@ -10,3 +13,22 @@ def test_can_get_summary_for_exam(client):
     )
     assert summary["rubric"]["questions_to_answer"] == 3
     assert summary["begins"] == "2019-01-01T08:00:00Z"
+
+
+@pytest.mark.parametrize(
+    "exam_id, expected_questions", [("simple", 1), ("multiple_questions", 2)]
+)
+def test_can_get_questions_for_exam(client, exam_id, expected_questions):
+    res = client(exam_id).get("/questions")
+    assert res.status_code == 200
+    assert len(res.json()) == expected_questions
+
+
+def test_questions_for_exam_have_expected_fields(client):
+    res = client("simple").get("/questions")
+    assert res.status_code == 200
+    questions = res.json()
+    assert questions["1"]["title"] == "Title of the question"
+    assert questions["1"]["instructions"] == "Some instructions for this question."
+    assert questions["1"]["show_part_weights"] is False
+    assert len(questions["1"]["parts"]) == 1
