690bdd6db8b0a3fd324632d03dc1d2ad09b72503
jsbailey <jason.bailey@imperial.ac.uk>
2024-06-13 16:45:11 +0100

    test: simplify test for answers

--- tests/api/test_answers.py
+++ tests/api/test_answers.py
@@ -1,11 +1,8 @@
 def test_can_get_all_student_answers_for_exam(client, answer_factory):
     answer_factory.create_batch(
-        size=4, exam_id="y2023_12345_exam", question=1, username="hpotter"
-    )
-    answer_factory.create_batch(
-        size=5, exam_id="y2023_12345_exam", question=2, username="hpotter"
+        size=5, exam_id="y2023_12345_exam", question=1, username="hpotter"
     )
 
     res = client("y2023_12345_exam").get("/answers/hpotter")
     assert res.status_code == 200
-    assert len(res.json()) == 9
+    assert len(res.json()) == 5
