30f110efb26122a28ceb80311242b7dde1f86870
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-07-02 11:58:38 +0100

    feat: implement QPS query

--- api/router/answers.py
+++ api/router/answers.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlmodel import Session, select
+
+from api.dependencies import get_assessment_id, get_session
+from api.models.answer import Answer
 
 answers_router = APIRouter(prefix="/answers/{username}", tags=["exam"])
 
@@ -15,5 +19,18 @@ def get_qpsa(
     question_number: int,
     part_number: int,
     section_number: int,
+    session: Session = Depends(get_session),
+    assessment_id: str = Depends(get_assessment_id),
 ):
-    return []
+    query = (
+        select(Answer)
+        .where(
+            Answer.exam_id == assessment_id,
+            Answer.question == question_number,
+            Answer.part == part_number,
+            Answer.section == section_number,
+            Answer.username == username,
+        )
+        .order_by(Answer.task)  # type: ignore
+    )
+    return session.exec(query).all()
--- tests/api/test_answers.py
+++ tests/api/test_answers.py
@@ -4,7 +4,10 @@
 from api.factories.student import StudentFactory
 
 
-def test_can_get_a_part_for_student(client, student_factory: StudentFactory):
-    student_ = student_factory(exam_id="simple")
-    res = client("random").get("/answers/hpotter/question/1/part/1/section/1")
+def test_can_get_a_part_for_student(client, answer_factory):
+    answer_factory(
+        exam_id="y2023_12345_exam", question=1, part=1, section=1, username="hpotter"
+    )
+    res = client("y2023_12345_exam").get("/answers/hpotter/question/1/part/1/section/1")
     assert res.status_code == 200
+    assert len(res.json()) == 1
