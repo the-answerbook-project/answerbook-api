83217cb272e1dd12100f116fc75b3a365468c46c
jsbailey <jason.bailey@imperial.ac.uk>
2024-07-02 20:17:27 +0100

    feat: populate dev db with fake answers

--- cli.py
+++ cli.py
@@ -47,9 +47,23 @@ def populate_db(
     Populates the database with dummy data.
     """
     with dynamic_session():
-        # StudentFactory(username="hpotter")
+
         StudentFactory(username="hgranger", exam_id="y2023_12345_exam")
-        # AnswerFactory(username="hpotter", question=1, part=1, section=1)
+        StudentFactory(username="hpotter", exam_id="y2023_12345_exam")
+        AnswerFactory(username="hpotter", question=1, part=1, section=1, task=1)
+        AnswerFactory(username="hpotter", question=1, part=1, section=1, task=2)
+        AnswerFactory(username="hpotter", question=1, part=1, section=1, task=3)
+        AnswerFactory(username="hpotter", question=2, part=1, section=1, task=1)
+        AnswerFactory(username="hpotter", question=2, part=1, section=1, task=1)
+        AnswerFactory(username="hpotter", question=3, part=1, section=1, task=1)
+
+        StudentFactory(username="hgranger", exam_id="y2023_12345_exam")
+        AnswerFactory(username="hgranger", question=1, part=1, section=1, task=1)
+        AnswerFactory(username="hgranger", question=1, part=1, section=1, task=2)
+        AnswerFactory(username="hgranger", question=1, part=1, section=1, task=3)
+        AnswerFactory(username="hgranger", question=2, part=1, section=1, task=1)
+        AnswerFactory(username="hgranger", question=2, part=1, section=1, task=1)
+        AnswerFactory(username="hgranger", question=3, part=1, section=1, task=1)
     print("Database populated successfully.")
 
 
