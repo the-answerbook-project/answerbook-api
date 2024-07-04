22a08bcacec2bda7afc7e76242b40431909b4631
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-07-04 16:24:15 +0100

    feat: automarker pipeline (#27)

--- scripts/apply_automarker.py
+++ scripts/apply_automarker.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 import requests
 from automarker_types import Automarker
 
@@ -7,22 +9,45 @@ def mark_students(
     part_no: int,
     section_no: int,
     exam_id: str,
-    automarker: Automarker,
+    automarkers: List[Automarker],
     max_mark: int,
+    test_mode: bool,
+    limit: Optional[int],
 ):
-    students = get_students()
+    students = get_students(limit)
 
     for student in students:
         tasks = get_student_answer(student, question_no, part_no, section_no, exam_id)
 
-        mark_res = automarker(tasks, max_mark)
+        for automarker in automarkers:
+            mark_res = automarker(tasks, max_mark)
 
-        if mark_res is not None:
-            (mark, feedback) = mark_res
-            post_feedback(student, mark, feedback, question_no, part_no, section_no)
+            if mark_res is not None:
+                (mark, feedback) = mark_res
+
+                if test_mode:
+                    print()
+                    print("Student answer:")
+                    print(
+                        "<No attempt>"
+                        if not tasks
+                        else "\n".join(
+                            [f"Task {task["task"]}: {task["answer"]}" for task in tasks]
+                        )
+                    )
+                    print()
+                    print(f"Mark: {mark} / {max_mark}")
+                    print(f"Feedback: {feedback}")
+                    print()
+                else:
+                    post_feedback(
+                        student, mark, feedback, question_no, part_no, section_no
+                    )
+
+                break
 
 
-def get_student_answer(student, question_no, part_no, section_no, exam_id):
+def get_student_answer(student, question_no, part_no, section_no, _exam_id):
     res = requests.get(
         f"http://localhost:5004/answers/{student['username']}/question/{question_no}/part/{part_no}/section/{section_no}"
     )
@@ -43,6 +68,6 @@ def post_feedback(student, mark, feedback, question_no, part_no, section_no):
     )
 
 
-def get_students():
+def get_students(limit: Optional[int]):
     res = requests.get("http://localhost:5004/students")
-    return res.json()
+    return res.json()[:limit] if limit is not None else res.json()
